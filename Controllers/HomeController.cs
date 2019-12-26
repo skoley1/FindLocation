bcec98dbf9ecf37b1d58b01463c41c1305aedc83
SUDEEP KOLEY <sudkoley@in.ibm.com>
2019-12-26 18:28:04 +0530

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using locationfinder.Models;
+
+namespace locationfinder.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            LocationLists model = new LocationLists();
+            var locations = new List<Locations>()
+            {
+                new Locations(1, "IBM DLF1","IBM DLF1, Kolkata", 22.581431, 88.450462),
+                new Locations(2, "IBM BCS","IBM BCS, Kolkata", 22.5718, 88.4346),
+                new Locations(3, "Bengaluru","Bengaluru, Karnataka", 12.972442, 77.580643)
+            };
+            model.LocationList = locations;
+            return View(model);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace locationfinder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
